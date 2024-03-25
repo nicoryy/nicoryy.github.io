fd37b34cd5bf6d0f045f55dbd641e15d4eff005c
Nicory <pedronicory@gmail.com>
2024-03-24 21:38:35 -0300

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import logo from '../assets/p-p.png'
+import Nav from './components/Nav'
 
 function App() {
 
   return (
     <>
-      <div className="grid place-items-center">
-        
+      <div className="bg-[#170a1a] h-96">
+        <Nav/>
       </div>
     </>
   )
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,22 +1,34 @@
-import React from 'react'
-import hlogo from '../../assets/w-logo.png'
+import React from "react";
+import hlogo from "../../assets/w-logo.png";
 
 const Nav = () => {
   return (
-    <div>
-        <div className="grid">
-            
-        </div>
+    <header>
+        <nav>
+            <div className="px-20 py-5 flex justify-between bg-blue-400">                                                                                
 
-        <div className="flex">
+            <div className="flex items-center bg-red-300">
+                <img className="w-56" src={hlogo} alt="horizontal_logo" />
+            </div>
 
-        </div>
+            <div className="grid place-items-center bg-red-300">
+                <ul className="flex gap-10 items-center">
+                <li>Welcome</li>
+                <li>About Me</li>
+                <li>Portfolio</li>
+                <li>Services</li>
+                <li>Experience</li>
+                </ul>
+            </div>
 
-        <div className="grid">
+            <div className="grid bg-red-300">
+                <button>Contact</button>
+            </div>
 
-        </div>
-    </div>
-  )
-}
+            </div>
+        </nav>
+    </header>
+  );
+};
 
-export default Nav
+export default Nav;
