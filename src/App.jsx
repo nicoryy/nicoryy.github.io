1892ef067b7eca579eff6c4a269007aad6307920
nicory <pedronicory@gmail.com>
2024-03-31 19:39:47 -0300

    better html sintaxe

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,29 @@
-import Nav from './components/Nav'
+import Nav from "./components/Nav";
 
 function App() {
-
   return (
     <>
-      <div className="bg-[#170a1a] px-20">
-        <Nav/>
+      <section className="bg-[#170a1a] px-20">
+        <Nav />
 
-        <main className='text-white'>
+        <main className="text-white">
           <div className="h-[calc(100vh-120px)] flex flex-col items-start justify-center pb-[120px]">
-            <h2 className='text-gray-600 font-bold text-5xl'>
-              I'm a
-            </h2>
+            <h2 className="text-gray-600 font-bold text-5xl">I'm a</h2>
 
             <h1 className='font-bold text-7xl tracking-wider after:content-["."]'>
               Full Stack <br />
               Website <br />
-              Developer 
+              Developer
             </h1>
 
-            <button className='bg-red-300'>Previous Projects</button>
+            <button className="bg-red-300">Previous Projects</button>
           </div>
 
-          <div className="h-fullvh">
-            Abortar
-          </div>
+          <div className="h-fullvh">Abortar</div>
         </main>
-      </div>
+      </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,23 +4,31 @@ import hlogo from "../../assets/w-logo.png";
 const Nav = () => {
   return (
     <header className="max-h-[120px]">
-        <nav>
-            <div className="py-5 flex justify-between text-white uppercase">                                                                                
+      <nav className="py-5 flex items-center mx-auto justify-between text-white uppercase">
+        <img className="w-56 " src={hlogo} alt="horizontal_logo" />
 
-                <img className="w-56 " src={hlogo} alt="horizontal_logo" />
+        <ul className="flex gap-10  items-center">
+          <li>
+            <a href="#welcome">Welcome</a>
+          </li>
+          <li>
+            <a href="#About">About Me</a>
+          </li>
+          <li>
+            <a href="#Portfolio">Portfolio</a>
+          </li>
+          <li>
+            <a href="#Services">Services</a>
+          </li>
+          <li>
+            <a href="#Experience">Experience</a>
+          </li>
+        </ul>
 
-                <ul className="flex gap-10  items-center">
-                  <li><a href="#welcome">Welcome</a></li>
-                  <li><a href="#About">About Me</a></li>
-                  <li><a href="#Portfolio">Portfolio</a></li>
-                  <li><a href="#Services">Services</a></li>
-                  <li><a href="#Experience">Experience</a></li>
-                </ul>
-
-                <button className=" uppercase my-auto py-3 px-8 bg-purple-900 rounded-md">Contact</button>
-                
-            </div>
-        </nav>
+        <button className=" uppercase my-auto py-3 px-8 bg-purple-900 rounded-md">
+          Contact
+        </button>
+      </nav>
     </header>
   );
 };
