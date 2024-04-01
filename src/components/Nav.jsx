40f9141abab50142969623ff82f9df23e3637c6e
nicory <pedronicory@gmail.com>
2024-04-01 14:27:58 -0300

    header resposive

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,33 +1,61 @@
 import React from "react";
 import hlogo from "../../assets/w-logo.png";
+import { IoIosClose, IoIosMenu } from "react-icons/io";
+
 
 const Nav = () => {
+  function handleNavMenu(){
+    const menu = document.querySelector("#menu")
+    const close = document.querySelector('#close')
+    const nav = document.querySelector('#nav')
+
+    nav.classList.toggle('top-[20%]')
+    menu.classList.toggle('hidden')
+    close.classList.toggle('hidden')
+
+  }
+
   return (
-    <header className="max-h-[120px]">
-      <nav className="py-5 flex items-center mx-auto justify-between text-white uppercase">
-        <img className="w-56 " src={hlogo} alt="horizontal_logo" />
+    <header className="max-h-[120px] w-[calc(100vw-10rem)]">
+      <nav className=" py-5 flex items-center mx-auto justify-between text-white uppercase">
+        <img className="w-56" src={hlogo} alt="horizontal_logo" />
 
-        <ul className="flex gap-10  items-center">
-          <li>
-            <a href="#welcome">Welcome</a>
-          </li>
-          <li>
-            <a href="#About">About Me</a>
-          </li>
-          <li>
-            <a href="#Portfolio">Portfolio</a>
-          </li>
-          <li>
-            <a href="#Services">Services</a>
-          </li>
-          <li>
-            <a href="#Experience">Experience</a>
-          </li>
-        </ul>
+        <section
+          id="nav"
+          className="lg:static lg:min-h-fit absolute
+          lg:bg-transparent bg-purple-600 min-h-[60vh] 
+          left-0 top-[-100%] lg:w-auto w-[calc(100vw-10rem)]
+          flex items-center mx-20 justify-center
+          transition
+          duration-300
+            "
+        >
+          <ul className="flex lg:gap-10 gap-8 lg:flex-row flex-col lg:items-center ">
+            <li>
+              <a className="hover:text-purple-900 duration-300" href="#Welcome">Welcome</a>
+            </li>
+            <li>
+              <a className=" hover:text-purple-900 duration-300 text-nowrap" href="#About">About Me</a>
+            </li>
+            <li>
+              <a className="hover:text-purple-900 duration-300" href="#Portfolio">Portfolio</a>
+            </li>
+            <li>
+              <a className="hover:text-purple-900 duration-300" href="#Services">Services</a>
+            </li>
+            <li>
+              <a className="hover:text-purple-900 duration-300" href="#Experience">Experience</a>
+            </li>
+          </ul>
+        </section>
 
-        <button className=" uppercase my-auto py-3 px-8 bg-purple-900 rounded-md">
-          Contact
-        </button>
+        <section className="flex gap-5">
+          <button className="hover:text-purple-900 hover:bg-purple-200 duration-300 uppercase my-auto py-3 px-8 bg-purple-900 rounded-lg">
+            Contact
+          </button>
+          <IoIosMenu id="menu" onClick={(e)=>handleNavMenu(e)} size={40} className="cursor-pointer lg:hidden"/>
+          <IoIosClose id="close" onClick={(e)=>handleNavMenu(e)} size={40} className="cursor-pointer hidden lg:hidden"/>
+        </section>
       </nav>
     </header>
   );
