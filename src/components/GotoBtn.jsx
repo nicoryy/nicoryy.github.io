5c5ffcf3324794e368589721bf90a0100e187c2d
Nicory <pedronicory@gmail.com>
2024-04-13 21:04:23 -0300

    |Portfolio content

--- src/components/GotoBtn.jsx
+++ src/components/GotoBtn.jsx
@@ -8,7 +8,7 @@ const GotoBtn = (props) => {
       hover:bg-purple-200 hover:to-purple-300
       duration-500 uppercase sm:py-3 sm:px-8 
       bg-gradient-to-tl from-purple-500 to-purple-900 
-      rounded-lg 2xs:py-2 2xs:px-7  ${props.class}`}
+      rounded-lg 2xs:py-2 2xs:px-7 ${props.class}`}
     >
       {props.children}
     </a>
--- src/components/TabPortfolio.jsx
+++ src/components/TabPortfolio.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import pic from "../../assets/TravelUi App.png";
+import GotoBtn from "./GotoBtn";
 
 const TabPortfolio = () => {
   const [activeTab, setActiveTab] = useState(1);
@@ -9,7 +11,7 @@ const TabPortfolio = () => {
   return (
     <section className="h-[100vh] ">
       {/* selection tab */}
-      <div>
+      <div className="mb-16">
         <ul className="flex gap-10 justify-center">
           <li
             onClick={() => {
@@ -50,6 +52,28 @@ const TabPortfolio = () => {
           </li>
         </ul>
       </div>
+
+      {/* content tab */}
+      <section className="flex">
+        <section className="grid place-items-center">
+          <img className="w-[70%] rounded-3xl" src={pic} alt="imgpic" />
+        </section>
+
+        <section className="flex flex-col text-left justify-between">
+          <span className="font-bold text-3xl">Project 1</span>
+
+          <span className="w-[80%] text-justify">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, ab?
+            Corrupti, facilis repellat eum voluptates enim asperiores modi quis.
+            Debitis asperiores sit neque hic, delectus esse qui provident odit?
+            Dolorem.
+          </span>
+
+          <span>
+            <GotoBtn>Read More</GotoBtn>
+          </span>
+        </section>
+      </section>
     </section>
   );
 };
