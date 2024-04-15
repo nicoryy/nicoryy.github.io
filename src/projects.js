31d33c484b4bd106b7bf9648ce18d6a44c394046
nicoryy <pedronicory@gmail.com>
2024-04-15 00:32:07 -0300

    added website project

--- src/projects.js
+++ src/projects.js
@@ -2,6 +2,8 @@ import travel from "../assets/TravelUi App.png";
 import todo from "../assets/ToDo React.png";
 import elchancho from "../assets/elChancho.png";
 
+import password from '../assets/Password Generator React.png'
+
 export const mobile = [
   {
     id: 1,
@@ -28,20 +30,20 @@ export const mobile = [
 
 export const website = [
   {
-    id: 3,
-    pic: elchancho,
-    title: "ElChancho Cumim App",
-    desc: "An original ideia to make an app to take orders of costumers for Waitress helper that hasn't the Order Machine. The main purpose was that it makes it easier to receive orders on busy days, where writing on paper would take a lot of time for larger orders. Unfortunally it isn't fully completed because i've been fired. ",
-    link: "https://github.com/nicoryy/el_chancho_menu",
+    id: 2,
+    pic: password,
+    title: "Password Generator React",
+    desc: " Is an application that allows users to generate secure passwords based on certain criteria. This type of project typically includes features such as the ability to specify the password length, choose which types of characters (uppercase letters, lowercase letters, numbers, and special characters) should be included, and generate a password based on the chosen options.",
+    link: "https://github.com/nicoryy/password_generator_react",
   },
 ];
 
 export const desktop = [
   {
     id: 2,
-    pic: todo,
-    title: "To Do App",
-    desc: "Most Front than Back project. It is a simple ToDo app that i made it in a FullStack Course By InfinitySchool. Project it is not fully completed. Basic Ui Design to have a simple apparence and easy to use.",
-    link: "https://github.com/nicoryy/To-do-List-React-Native",
+    pic: password,
+    title: "Password Generator React",
+    desc: " Is an application that allows users to generate secure passwords based on certain criteria. This type of project typically includes features such as the ability to specify the password length, choose which types of characters (uppercase letters, lowercase letters, numbers, and special characters) should be included, and generate a password based on the chosen options.",
+    link: "https://github.com/nicoryy/password_generator_react",
   },
 ];
