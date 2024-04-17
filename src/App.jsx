0ba7538a6e49613faeff9fd2cbf88386d697cecf
nicoryy <pedronicory@gmail.com>
2024-04-17 13:32:15 -0300

    Experience Section

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,18 @@ import Nav from "./components/Nav";
 import profile from "../assets/profile20000.png";
 import TabPortfolio from "./components/TabPortfolio";
 import { mobile, website, desktop } from "./projects";
-import { useState } from "react";
+import React, { useState } from "react";
 import Cards from "./components/Cards";
 import Footer from "./components/Footer";
 import imgmobile from "../assets/mobile.png";
+import react from "../assets/atom.png";
+import node from "../assets/node-js.png";
+import tail from "../assets/icons8-tailwind-css-512.png";
+import express from "../assets/icons8-expresso-js-512.png";
+import python from "../assets/python.png";
+import mongodb from "../assets/icons8-mongodb-512.png";
+import native from "../assets/development.png";
+import js from "../assets/js.png";
 
 function App() {
   const [activeTab, setActiveTab] = useState(1);
@@ -92,7 +100,12 @@ function App() {
                   Hire
                 </GotoBtn>
 
-                <GotoBtn goto='https://www.linkedin.com/in/nicoryy' class="px-[15%] ">Resume</GotoBtn>
+                <GotoBtn
+                  goto="https://www.linkedin.com/in/nicoryy"
+                  class="px-[15%] "
+                >
+                  Resume
+                </GotoBtn>
               </section>
             </section>
           </section>
@@ -162,6 +175,45 @@ function App() {
             <p className="text-3xl uppercase text-center font-bold">Services</p>
 
             <Cards />
+          </section>
+
+          {/* EXPERIENCE SECTION */}
+          <section id='Experience' className="text-center my-20">
+            <p className="font-bold text-3xl uppercase mb-32"> experiences</p>
+            <ul className="grid place-items-center md:grid-cols-4 2xs:grid-cols-2 gap-20">
+              <li className="flex flex-col gap-2 items-center">
+                <img className="size-16" src={react} alt="reactimg" />
+                <p className="text-gray-400 text-xl">React.Js</p>
+              </li>
+              <li className="flex flex-col gap-2 items-center">
+                <img className="size-16" src={node} alt="reactimg" />
+                <p className="text-gray-400 text-xl">Node.Js</p>
+              </li>
+              <li className="flex flex-col gap-2 items-center">
+                <img className="size-16" src={express} alt="reactimg" />
+                <p className="text-gray-400 text-xl">Express.Js</p>
+              </li>
+              <li className="flex flex-col gap-2 items-center">
+                <img className="size-16" src={mongodb} alt="reactimg" />
+                <p className="text-gray-400 text-xl">MongoDb</p>
+              </li>
+              <li className="flex flex-col gap-2 items-center">
+                <img className="size-16" src={python} alt="reactimg" />
+                <p className="text-gray-400 text-xl">Python</p>
+              </li>
+              <li className="flex flex-col gap-2 items-center">
+                <img className="size-16" src={native} alt="reactimg" />
+                <p className="text-gray-400 text-xl">React Native</p>
+              </li>
+              <li className="flex flex-col gap-2 items-center">
+                <img className="size-16" src={tail} alt="reactimg" />
+                <p className="text-gray-400 text-xl">Tailwind Css</p>
+              </li>
+              <li className="flex flex-col gap-2 items-center">
+                <img className="size-16" src={js} alt="reactimg" />
+                <p className="text-gray-400 text-xl">JavaScript</p>
+              </li>
+            </ul>
           </section>
         </main>
         <Footer />
