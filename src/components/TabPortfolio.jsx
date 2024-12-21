231d9230161c01dfd7aa7e340bf8b38537b4c600
Nicory <pedronicory@gmail.com>
2024-12-21 11:53:02 -0300

    "feat: Refactor TabPortfolio - Use props data consistency"

--- src/components/TabPortfolio.jsx
+++ src/components/TabPortfolio.jsx
@@ -2,53 +2,59 @@ import React, { useState } from "react";
 import GotoBtn from "./GotoBtn";
 
 const TabPortfolio = (props) => {
+  const { data } = props;
+
+  if (!data) {
+    throw new Error("No data provided to TabPortfolio component");
+  }
+
   return (
     <section className="min-h-[100vh] ">
       {/* content tab */}
-      {props.data.map((project) => (
+      {data.map((project, index) => (
         <section
-          key={project.id}
+          key={index}
           className={
-            project.id % 2 === 0
+            index % 2 === 0
               ? " flex lg:flex-row-reverse mb-10 gap-10 lg:mx-20 mx-2 flex-col"
               : " flex lg:flex-row mb-10 gap-10 lg:mx-20 mx-2 flex-col"
           }
         >
           <img
-            src={project.pic}
+            src={project?.profileImage}
             alt="imgpic"
             className="lg:w-[30rem] rounded-lg drop-shadow-3xl w-full"
           />
 
           <div
             className={
-              project.id % 2 === 0
+              index % 2 === 0
                 ? "flex flex-col items-end justify-between"
                 : "flex flex-col items-start justify-between"
             }
           >
             <div
               className={
-                project.id % 2 === 0
+                index % 2 === 0
                   ? "flex flex-col gap-1 items-end"
                   : "flex flex-col gap-1 items-start"
               }
             >
               <span className="text-2xl font-thin uppercase text-purple-700">
-                Project {project.id}
+                Project {index + 1}
               </span>
-              <span className="text-3xl font-bold">{project.title}</span>
+              <span className="text-3xl font-bold">{project?.name}</span>
             </div>
             <span
               className={
-                project.id % 2 === 0
+                index % 2 === 0
                   ? "lg:w-96 my-5 lg:my-0 text-right md:text-sm lg:text-base text-gray-400 w-full"
                   : "lg:w-96 my-5 lg:my-0 text-left md:text-sm lg:text-base text-gray-400 w-full"
               }
             >
-              {project.desc}
+              {project?.description}
             </span>
-            <GotoBtn goto={project.link}>Read more</GotoBtn>
+            <GotoBtn goto={project?.url}>Visit it!</GotoBtn>
           </div>
         </section>
       ))}
