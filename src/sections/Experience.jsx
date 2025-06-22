8498bd42b9e345c2fc5ea1414851309dfed7b490
Nicory <pedronicory@gmail.com>
2025-06-22 16:53:52 -0300

    Refactor code structure for improved readability and maintainability

--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -1,13 +1,14 @@
-import react from "../../assets/atom.png";
-import node from "../../assets/node-js.png";
-import tail from "../../assets/icons8-tailwind-css-512.png";
-import express from "../../assets/icons8-expresso-js-512.png";
-import python from "../../assets/python.png";
-import mongodb from "../../assets/icons8-mongodb-512.png";
-import native from "../../assets/sdk-react-native.svg";
-import js from "../../assets/js.png";
-import ts from "../../assets/icons8-typescript-96.png";
-import vercel from "../../assets/vercel-logo-svgrepo-com.svg";
+import react from "../../assets/experiences/react.png";
+import node from "../../assets/experiences/node.png";
+import express from "../../assets/experiences/express.png";
+import python from "../../assets/experiences/python.png";
+import mongodb from "../../assets/experiences/mongodb.png";
+import native from "../../assets/experiences/react-native.svg";
+import ts from "../../assets/experiences/typescript.png";
+import vercel from "../../assets/experiences/vercel.svg";
+import next from "../../assets/experiences/nextjs.png";
+import supabase from '../../assets/experiences/supabase.png';
+import shadcn from '../../assets/experiences/shadcn-ui.png';
 
 const Experience = () => {
   return (
@@ -15,43 +16,43 @@ const Experience = () => {
       <p className="font-bold text-3xl uppercase mb-32"> experiences</p>
       <ul className="grid place-items-center lg:grid-cols-4 grid-cols-2 gap-20">
         <li className="flex flex-col gap-2 items-center">
-          <img className="size-16" src={react} alt="reactimg" />
-          <p className="text-gray-400 text-xl">React.Js</p>
+          <img className="size-16" src={next} alt="nextjs" />
+          <p className="text-gray-400 text-xl">Next Js</p>
         </li>
         <li className="flex flex-col gap-2 items-center">
-          <img className="size-16" src={node} alt="reactimg" />
-          <p className="text-gray-400 text-xl">Node.Js</p>
+          <img className="size-16" src={supabase} alt="supabase" />
+          <p className="text-gray-400 text-xl">Supabase</p>
         </li>
         <li className="flex flex-col gap-2 items-center">
-          <img className="size-16" src={express} alt="reactimg" />
-          <p className="text-gray-400 text-xl">Express.Js</p>
+          <img className="size-16 bg-white/70 rounded-3xl p-3" src={shadcn} alt="shadcn" />
+          <p className="text-gray-400 text-xl">ShadCn</p>
         </li>
         <li className="flex flex-col gap-2 items-center">
-          <img className="size-16" src={mongodb} alt="reactimg" />
-          <p className="text-gray-400 text-xl">MongoDb</p>
-        </li>
-        <li className="flex flex-col gap-2 items-center">
-          <img className="size-16" src={python} alt="reactimg" />
-          <p className="text-gray-400 text-xl">Python</p>
-        </li>
-        <li className="flex flex-col gap-2 items-center">
-          <img className="size-16" src={native} alt="reactimg" />
-          <p className="text-gray-400 text-xl">React Native</p>
-        </li>
-        <li className="flex flex-col gap-2 items-center">
-          <img className="size-16" src={tail} alt="reactimg" />
-          <p className="text-gray-400 text-xl">Tailwind Css</p>
-        </li>
-        <li className="flex flex-col gap-2 items-center">
-          <img className="size-16" src={js} alt="reactimg" />
-          <p className="text-gray-400 text-xl">JavaScript</p>
-        </li>
-        <li className="flex flex-col gap-2 items-center">
-          <img className="size-16" src={ts} alt="reactimg" />
+          <img className="size-16" src={ts} alt="typescript" />
           <p className="text-gray-400 text-xl">TypeScript</p>
         </li>
         <li className="flex flex-col gap-2 items-center">
-          <img className="size-16" src={vercel} alt="reactimg" />
+          <img className="size-16" src={native} alt="reactnative" />
+          <p className="text-gray-400 text-xl">React Native</p>
+        </li>
+        <li className="flex flex-col gap-2 items-center">
+          <img className="size-16" src={mongodb} alt="mongodb" />
+          <p className="text-gray-400 text-xl">MongoDb</p>
+        </li>
+        <li className="flex flex-col gap-2 items-center">
+          <img className="size-16" src={python} alt="python" />
+          <p className="text-gray-400 text-xl">Python</p>
+        </li>
+        <li className="flex flex-col gap-2 items-center">
+          <img className="size-16" src={node} alt="nodejs" />
+          <p className="text-gray-400 text-xl">Node.Js</p>
+        </li>
+        <li className="flex flex-col gap-2 items-center">
+          <img className="size-16" src={express} alt="expressjs" />
+          <p className="text-gray-400 text-xl">Express.Js</p>
+        </li>
+        <li className="flex flex-col gap-2 items-center">
+          <img className="size-16" src={vercel} alt="vercel" />
           <p className="text-gray-400 text-xl">Vercel</p>
         </li>
       </ul>
